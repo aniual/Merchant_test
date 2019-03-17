1b1fddde739fa728909bbca51e5301b4bdbacef6
hz <hz@hzdeMac-mini.lan>
2019-03-17 22:55:49 +0800

    add gamelist

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	_ "Merchants_test/routers"
 	"github.com/astaxie/beego"
+	"Merchants_test/controllers"
 )
 
 func main() {
 	beego.BConfig.WebConfig.Session.SessionOn=true
+	beego.ErrorController(&controllers.ErrorController{})
 	beego.Run()
 }
 
